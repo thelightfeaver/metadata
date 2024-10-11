aa2c6042d4b4009f44d8dc317fdef50fbae30899
diogenes69 <diogenes_monegro@claro.com.do>
2024-10-11 12:47:08 -0400

    Add initial project structure with metadata handling and tests

---
+++ src/metadata.py
@@ -0,0 +1,35 @@
+from docx import Document
+from pprint import pprint
+
+
+class Metadata:
+
+    def __init__ (self, filename):
+        self._filename = filename
+        self._structures = list()
+        self.get_data()
+
+    def read_docx_metadata(self):
+        doc = Document(self._filename)
+        mt = dict()
+        for attr in self._structures:
+           mt[attr] = getattr(doc.core_properties, attr)
+        return mt
+    
+    def get_data(self): 
+        doc = Document(self._filename)
+        for attr in dir(doc.core_properties):
+            if not callable(attr) and( not str(attr).startswith('__') and not str(attr).endswith('_')):
+                self._structures.append(attr)
+
+    def write_docx_metadata(self, data):
+        doc = Document(self._filename)
+        for attr in self._structures:
+            setattr(doc.core_properties, attr, data[attr])
+        doc.save(self._filename)
+        
+    
+if __name__ == "__main__":
+    metadata = Metadata('src/data/file.docx')
+    pprint(metadata.read_docx_metadata())
+    metadata.write_docx_metadata({'title': 'algo', 'author': 'author'})
---
+++ src/tests/test_main.py
@@ -0,0 +1,45 @@
+from src.metadata import Metadata
+
+import pytest
+
+
+def test_read_doc():
+    URL = "src/data/file.docx"
+    STRUCTURES = [
+        "author",
+        "category",
+        "comments",
+        "content_status",
+        "created",
+        "identifier",
+        "keywords",
+        "language",
+        "last_modified_by",
+        "last_printed",
+        "modified",
+        "revision",
+        "subject",
+        "title",
+        "version",
+    ]
+    mt = Metadata(URL)
+    results = mt.read_docx_metadata()
+    assert len(results.items()) > 0
+    assert STRUCTURES in results.keys()
+
+
+def test_write_doc():
+    URL = "src/data/file.docx"
+    mt = Metadata(URL)
+
+    data = {"title": "algo", "author": "author"}
+
+    mt.write_docx_metadata(data)
+
+    results = mt.read_docx_metadata()
+
+    assert results["title"] == "algo"
+
+    assert results["author"] == "author"
+
+    assert len(results.items()) > 0
